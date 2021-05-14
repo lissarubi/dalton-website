472f46ec3fde3f8905c7bc1719514746370772bd
Ederson Ferreira <edersondeveloper@gmail.com>
2021-05-14 11:29:16 -0400

    add dist

---
+++ precache-manifest.8c6bfac36fd7062a49ec939beb79d46f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ee0c79df268921dcc9",
+    "url": "/dalton-website/css/app.cebb2c37.css"
+  },
+  {
+    "revision": "b7156611b379da6bae96",
+    "url": "/dalton-website/css/chunk-vendors.5d27a9d6.css"
+  },
+  {
+    "revision": "d5f0f941b60d788e4cefd3ba15dc0c25",
+    "url": "/dalton-website/img/daltonlogo.d5f0f941.png"
+  },
+  {
+    "revision": "cf2e6d97d1232c1a3897038d56009d07",
+    "url": "/dalton-website/index.html"
+  },
+  {
+    "revision": "f0ee0c79df268921dcc9",
+    "url": "/dalton-website/js/app.9b4ae636.js"
+  },
+  {
+    "revision": "b7156611b379da6bae96",
+    "url": "/dalton-website/js/chunk-vendors.9b02c8a7.js"
+  },
+  {
+    "revision": "1e59a24386b6e483d44a5b601e20c85a",
+    "url": "/dalton-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dalton-website/robots.txt"
+  }
+]);
